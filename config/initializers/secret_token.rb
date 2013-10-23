212d69a640c34dd358041ed3f3ae597e16e68c23
Alexander Efremov <alex17021991@gmail.com>
2013-10-23 15:26:29 +0600

    New rail application. POBEDITELI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'd592928e75f950a2199452d175e932ead1d7f001a19cd862dbf700aa971e121c0ac6dca6e59c68a4a682d1e824d6e69e143a994fde98e4f81eb2bb9387e4d582'
