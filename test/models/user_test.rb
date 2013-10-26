400a21e7473c37abaffa8b03884aebbdda11f3aa
Alexander Efremov <alex17021991@gmail.com>
2013-10-27 00:09:22 +0600

    added some validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,29 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "a user should enter first name" do
+        user = User.new
+        assert !user.save
+        assert !user.errors[:first_name].empty?
+    end
+    
+    test "a user should enter user last name" do
+        user = User.new
+        assert !user.save
+        assert !user.errors[:last_name].empty?
+    end
+    
+    test "a user should enter a profile name" do
+        user = User.new
+        assert !user.save
+        assert !user.errors[:profile_name].empty?
+    end
+    
+    test "a user should have a profile name without spaces" do
+        user = User.new
+        user.profile_name = "Profile with spaces"
+        assert !user.save
+        assert !user.errors[:profile_name].empty?
+        assert user.errors[:profile_name].include?("Must be formatted correctly.")
+    end
 end
